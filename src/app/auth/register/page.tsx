4875861ce53be4d392513a8f3d737fb73c31fe6c
thomas21-jquerry <thomasjose875@gmail.com>
2025-03-17 09:38:51 +0530

    done

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -29,7 +29,7 @@ export default function Register() {
         email,
         password,
         options: {
-          emailRedirectTo: `$${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+          emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
         },
       })
 
